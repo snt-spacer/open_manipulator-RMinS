f3c6aba81a70f1457561bd6e48d86e7b6042fc44
Andrej Orsula <orsula.andrej@gmail.com>
2026-08-01 13:39:20 +0200

    Update MoveIt launch
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- open_manipulator_moveit_config/launch/open_manipulator_x_moveit.launch.py
+++ open_manipulator_moveit_config/launch/open_manipulator_x_moveit.launch.py
@@ -18,7 +18,9 @@
 
 import os
 from pathlib import Path
+import yaml
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
@@ -35,89 +37,127 @@ def generate_launch_description():
     # Declare launch arguments
     declared_arguments = [
         DeclareLaunchArgument(
-            'start_rviz', default_value='true', description='Whether to execute rviz2'
+            "start_rviz", default_value="true", description="Whether to execute rviz2"
         ),
         DeclareLaunchArgument(
-            'use_sim',
-            default_value='false',
-            description='Whether to use simulation time',
+            "use_sim",
+            default_value="false",
+            description="Whether to use simulation time",
         ),
         DeclareLaunchArgument(
-            'warehouse_sqlite_path',
-            default_value=os.path.expanduser('~/.ros/warehouse_ros.sqlite'),
-            description='Path where the warehouse database should be stored',
+            "warehouse_sqlite_path",
+            default_value=os.path.expanduser("~/.ros/warehouse_ros.sqlite"),
+            description="Path where the warehouse database should be stored",
         ),
         DeclareLaunchArgument(
-            'publish_robot_description_semantic',
-            default_value='true',
-            description='Whether to publish robot description semantic',
+            "publish_robot_description_semantic",
+            default_value="true",
+            description="Whether to publish robot description semantic",
         ),
     ]
 
-    start_rviz = LaunchConfiguration('start_rviz')
-    use_sim = LaunchConfiguration('use_sim')
-    warehouse_sqlite_path = LaunchConfiguration('warehouse_sqlite_path')
-    publish_robot_description_semantic = LaunchConfiguration('publish_robot_description_semantic')
+    start_rviz = LaunchConfiguration("start_rviz")
+    use_sim = LaunchConfiguration("use_sim")
+    warehouse_sqlite_path = LaunchConfiguration("warehouse_sqlite_path")
+    publish_robot_description_semantic = LaunchConfiguration(
+        "publish_robot_description_semantic"
+    )
 
     moveit_config = (
         MoveItConfigsBuilder(
-            robot_name='open_manipulator_x', package_name='open_manipulator_moveit_config')
+            robot_name="open_manipulator_x",
+            package_name="open_manipulator_moveit_config",
+        )
+        .robot_description(mappings={"use_sim": use_sim})
         .robot_description_semantic(
-            str(Path('config') / 'open_manipulator_x' / 'open_manipulator_x.srdf'))
-        .joint_limits(str(Path('config') / 'open_manipulator_x' / 'joint_limits.yaml'))
+            str(Path("config") / "open_manipulator_x" / "open_manipulator_x.srdf")
+        )
+        .joint_limits(str(Path("config") / "open_manipulator_x" / "joint_limits.yaml"))
         .trajectory_execution(
-            str(Path('config') / 'open_manipulator_x' / 'moveit_controllers.yaml'))
+            str(Path("config") / "open_manipulator_x" / "moveit_controllers.yaml")
+        )
         .robot_description_kinematics(
-            str(Path('config') / 'open_manipulator_x' / 'kinematics.yaml'))
+            str(Path("config") / "open_manipulator_x" / "kinematics.yaml")
+        )
         .to_moveit_configs()
     )
+    robot_description = moveit_config.robot_description
 
     warehouse_ros_config = {
-        'warehouse_plugin': 'warehouse_ros_sqlite::DatabaseConnection',
-        'warehouse_host': warehouse_sqlite_path,
+        "warehouse_plugin": "warehouse_ros_sqlite::DatabaseConnection",
+        "warehouse_host": warehouse_sqlite_path,
     }
 
     move_group_node = Node(
-        package='moveit_ros_move_group',
-        executable='move_group',
-        output='screen',
+        package="moveit_ros_move_group",
+        executable="move_group",
+        output="screen",
         parameters=[
             moveit_config.to_dict(),
+            robot_description,
             warehouse_ros_config,
             {
-                'use_sim_time': use_sim,
-                'publish_robot_description_semantic': publish_robot_description_semantic,
+                "use_sim_time": use_sim,
+                "publish_robot_description_semantic": publish_robot_description_semantic,
             },
         ],
     )
 
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare('open_manipulator_moveit_config'), 'config', 'moveit.rviz']
-    )
+    rviz_config_file = PathJoinSubstitution([
+        FindPackageShare("open_manipulator_moveit_config"),
+        "config",
+        "moveit.rviz",
+    ])
     rviz_node = Node(
-        package='rviz2',
+        package="rviz2",
         condition=IfCondition(start_rviz),
-        executable='rviz2',
-        name='rviz2_moveit',
-        output='log',
-        arguments=['-d', rviz_config_file],
+        executable="rviz2",
+        name="rviz2_moveit",
+        output="log",
+        arguments=["-d", rviz_config_file],
         parameters=[
-            moveit_config.robot_description,
+            robot_description,
             moveit_config.robot_description_semantic,
             moveit_config.robot_description_kinematics,
             moveit_config.planning_pipelines,
             moveit_config.joint_limits,
             warehouse_ros_config,
             {
-                'use_sim_time': use_sim,
+                "use_sim_time": use_sim,
             },
         ],
     )
 
+    servo_yaml_path = (
+        Path(get_package_share_directory("open_manipulator_moveit_config"))
+        / "config"
+        / "open_manipulator_x"
+        / "servo.yaml"
+    )
+    with open(servo_yaml_path, "r", encoding="utf-8") as servo_yaml:
+        servo_params = {"moveit_servo": yaml.safe_load(servo_yaml)}
+
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_node",
+        name="servo_node",
+        output="screen",
+        parameters=[
+            servo_params,
+            {"update_period": 0.01},
+            {"planning_group_name": "arm"},
+            moveit_config.to_dict(),
+            robot_description,
+            {
+                "use_sim_time": use_sim,
+            },
+        ],
+    )
     return LaunchDescription(
         declared_arguments
         + [
             move_group_node,
             rviz_node,
+            servo_node,
         ]
     )
